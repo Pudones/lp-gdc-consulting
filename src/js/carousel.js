dccdfd65689b094e1fa0f353db49f185e02a0ee8
Pudones <110052317+Pudones@users.noreply.github.com>
2023-02-05 04:05:34 -0300

    - new sections added
    - testimonials and brand sections added.
    - jquery and slick carousel added.
    - image folders created for organization purposes.
    - drink water

---
+++ src/js/carousel.js
@@ -0,0 +1,14 @@
+$('.testimonials_carousel_wrapper').slick({
+  dots: true,
+  arrows: true,
+  autoplay: true,
+  autoplaySpeed: 1500,
+  infinite: true
+
+  // responsive: [{
+  //   breakpoint: 991,
+  //   settings: {
+  //     arrows: false
+  //   }
+  // }]
+});
